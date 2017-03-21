6c8f2a70eda048110de5f020d44352f3bdd39a82
Janos G. Hajagos <risk.limits@gmail.com>
2017-03-21 12:49:23 -0400

    Added skeleton files

---
+++ sql/create_denormalized_tables.sql
@@ -0,0 +1,7 @@
+--Inorder to map the tables to JSON and then to HDF5 flatten the table
+
+--create a denormalized tables for the main OHDSI table
+--convert date and time separately to date / time
+--convert date to Julian day
+
+
