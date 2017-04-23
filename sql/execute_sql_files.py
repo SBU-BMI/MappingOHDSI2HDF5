0bae43b72b13503455ce37c77fb53351511558c4
Janos G. Hajagos <risk.limits@gmail.com>
2017-04-23 07:57:58 -0400

    Added components for this script

--- sql/execute_sql_files.py
+++ sql/execute_sql_files.py
@@ -1,5 +1,38 @@
 import sqlalchemy as sa
-import json
-import os
+import sqlparse
+import argparse
 
+
+def main(sql_file_name, connection_string, schema=None):
+
+    engine = sa.create_engine(connection_string)
+    connection = engine.connect()
+
+    with open(sql_file_name) as f:
+
+        sql_txt = f.read()
+        sql_statements = sqlparse.split(sql_txt)
+
+        if schema is not None:
+            pre_statement = "set search_path=%s;" % schema
+        else:
+            pre_statement  = ""
+
+
+        for sql_statement in sql_statements:
+
+            print(sql_statement)
+            sql_to_execute = pre_statement + sql_statement
+            connection.exeute(sql_to_execute)
+
+
+if __name__ == "__main__":
+    arg_parser_obj = argparse.ArgumentParser()
+    arg_parser_obj.add_argument("-f", dest="file_name", default="execute_sql_files.py")
+    arg_parser_obj.add_argument("-c", dest="connection_string")
+    arg_parser_obj.add_argument("-s", dest="schema", default=None)
+
+
+    arg_obj = arg_parser_obj.parse_args()
+    main(sql_file_name=arg_obj.file_name, connection_string=arg_obj.connection_sting, schema=arg_obj.schema)
 
