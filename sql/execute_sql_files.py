6e21851c3a77e2a3438a496f6938f11784cef073
Janos G. Hajagos <risk.limits@gmail.com>
2017-04-25 18:12:23 -0400

    Spacing change

--- sql/execute_sql_files.py
+++ sql/execute_sql_files.py
@@ -16,8 +16,7 @@ def main(sql_file_name, connection_string, schema=None):
         if schema is not None:
             pre_statement = "set search_path=%s;" % schema
         else:
-            pre_statement  = ""
-
+            pre_statement = ""
 
         for sql_statement in sql_statements:
 
@@ -31,7 +30,6 @@ if __name__ == "__main__":
     arg_parser_obj.add_argument("-f", dest="file_name", default="execute_sql_files.py")
     arg_parser_obj.add_argument("-c", dest="connection_string")
     arg_parser_obj.add_argument("-s", dest="schema", default=None)
-
 
     arg_obj = arg_parser_obj.parse_args()
     main(sql_file_name=arg_obj.file_name, connection_string=arg_obj.connection_sting, schema=arg_obj.schema)
