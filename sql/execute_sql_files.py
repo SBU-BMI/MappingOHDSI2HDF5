33c23d327540e5583d7b2992bd269d6fa8a58ea5
janos <risk.limits@gmail.com>
2018-10-24 19:33:06 -0400

    Transaction support

--- sql/execute_sql_files.py
+++ sql/execute_sql_files.py
@@ -6,24 +6,29 @@ import argparse
 def main(sql_file_name, connection_string, schema=None):
 
     engine = sa.create_engine(connection_string)
-    connection = engine.connect()
+    with engine.connect() as connection:
 
-    with open(sql_file_name) as f:
+        with open(sql_file_name) as f:
 
-        sql_txt = f.read()
-        sql_statements = sqlparse.split(sql_txt)
+            sql_txt = f.read()
+            sql_statements = sqlparse.split(sql_txt)
 
-        if schema is not None:
-            pre_statement = "set search_path=%s;" % schema
-        else:
-            pre_statement = ""
+            if schema is not None:
+                pre_statement = "set search_path=%s;" % schema
+            else:
+                pre_statement = ""
 
-        for sql_statement in sql_statements:
+            for sql_statement in sql_statements:
 
-            print(sql_statement)
-            sql_to_execute = pre_statement + sql_statement
-            connection.execute(sql_to_execute)
-
+                print(sql_statement)
+                trans = connection.begin()
+                try:
+                    sql_to_execute = pre_statement + sql_statement
+                    connection.execute(sql_to_execute)
+                except:
+                    trans.rollback()
+                    raise
+                trans.commit()
 
 if __name__ == "__main__":
     arg_parser_obj = argparse.ArgumentParser()
